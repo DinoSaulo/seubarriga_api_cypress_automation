6dd7d3f0df38fd113a6e26424d07cdca5e563eee
dinosaulo <sab2@cin.ufpe.br>
2022-06-05 08:13:05 -0300

    minor: adding some tests

--- cypress/e2e/seubarriga/funcional.cy.js
+++ cypress/e2e/seubarriga/funcional.cy.js
@@ -34,4 +34,43 @@ describe('Should test at a funcional level', () => {
             expect(res.body.nome).to.be.equal("Conta via rest")
         })
     })
+
+    it('Should update an account', () => {
+        cy.request({
+            method: 'GET',
+            url: '/contas',
+            headers: {Authorization: `JWT ${token}`},
+            qs: {
+                nome: 'Conta para alterar'
+            }
+        }).then(res => {
+            cy.request({
+                method: 'PUT',
+                url: `/contas/${res.body[0].id}`,
+                headers: {Authorization: `JWT ${token}`},
+                body: {
+                    nome: "Conta alteradavia rest"
+                }
+            }).as('response')
+        })
+
+        cy.get('@response').its('status').should('be.equal', 200)
+    })
+
+    it('Should create an account with same name', () => {
+        cy.request({
+            method: 'POST',
+            url: '/contas',
+            failOnStatusCode: false, //para pegar o erro retornado
+            headers: {Authorization: `JWT ${token}`},
+            body: {
+                nome: 'Conta mesmo nome'
+            }
+        }).as('response')
+
+        cy.get('@response').then(res => {
+            expect(res.status).to.be.equal(400)
+            expect(res.body.error).to.be.equal("Já existe uma conta com esse nome!")
+        })
+    })
 })
